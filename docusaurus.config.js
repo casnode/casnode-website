dd3b44a96a5f863182d5983b57759f7f3ec99f02
ErikQQY <2283984853@qq.com>
2021-07-21 14:25:57 +0800

    feat: Add sitemap
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -170,6 +170,11 @@ module.exports = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        sitemap: {
+          changefreq: 'weekly',
+          priority: 0.5,
+          trailingSlash: false,
+        },
       },
     ],
   ],
