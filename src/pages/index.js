bb3f16ba1b9219a2243aaf0f106153eca8c427b7
ERIK <52615090+ErikQQY@users.noreply.github.com>
2021-08-11 22:54:08 +0800

    feat: Add Casnode iframe to page (#30)
    * feat: Add Casnode iframe to page
    
    Signed-off-by: ErikQQY <2283984853@qq.com>
    
    * feat: Add Casnode iframe to page
    
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
+import BrowserWindow from '../components/BrowserWindow';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -12,7 +13,9 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
+        <br />
         <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <br />
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
@@ -21,7 +24,26 @@ function HomepageHeader() {
           </Link>
         </div>
       </div>
+      <div className={styles.casnodewindow}>
+      <BrowserWindow url="https://forum.casbin.com" className={styles.BrowserWindow}>
+        <div className={styles.iframediv}>
+        <iframe className="headerwindow" src="https://forum.casbin.com" width="870px" height="450px" border="0" frameborder="0"></iframe>
+        </div>
+      </BrowserWindow>
+      </div>
     </header>
+  );
+}
+
+function CasnodeWindow() {
+  return (
+    <div className={styles.casnodewindow}>
+    <div className="container text--center">
+        <BrowserWindow url="https://forum.casbin.com">
+        <iframe src="https://forum.casbin.com" width="870" height="500" frameborder="0"></iframe>
+      </BrowserWindow>
+        </div>
+      </div>
   );
 }
 
