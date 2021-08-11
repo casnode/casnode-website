809930ab53699c937f3b2dab68951a71c9ef666b
ErikQQY <2283984853@qq.com>
2021-08-11 22:35:17 +0800

    feat: Add Casnode iframe to page
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,9 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
+        <br />
         <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <br />
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
@@ -21,6 +23,13 @@ function HomepageHeader() {
             Get Started
           </Link>
         </div>
+      </div>
+      <div className={styles.casnodewindow}>
+      <BrowserWindow url="https://forum.casbin.com" className={styles.BrowserWindow}>
+        <div className={styles.iframediv}>
+        <iframe className="headerwindow" src="https://forum.casbin.com" width="870px" height="450px" border="0" frameborder="0"></iframe>
+        </div>
+      </BrowserWindow>
       </div>
     </header>
   );
@@ -30,24 +39,11 @@ function CasnodeWindow() {
   return (
     <div className={styles.casnodewindow}>
     <div className="container text--center">
-      <div className="row">
-        <div className="col">
-          Casnode is a forum developed by <Link href="https://github.com/casbin/casnode">Casbin community</Link> with a modern interface.
-        <br />
-        With supporting many third-party application login, you can choose your favorite social network to login.
-        <br />
-        For more details about setup of Casnode, please visit <Link to="/docs/installation">Casnode installation</Link>
-        <br />
-        And if your have questions, you can ask questions in <Link href="https://github.com/casbin/casdoor">our Casnode community</Link>
-        </div>
-        <div>
         <BrowserWindow url="https://forum.casbin.com">
-        <iframe src="https://forum.casbin.com" width="850" height="500" frameborder="0"></iframe>
+        <iframe src="https://forum.casbin.com" width="870" height="500" frameborder="0"></iframe>
       </BrowserWindow>
         </div>
       </div>
-    </div>
-  </div>
   );
 }
 
@@ -60,7 +56,6 @@ export default function Home() {
       <HomepageHeader />
       <main>
         <HomepageFeatures />
-        <CasnodeWindow />
       </main>
     </Layout>
   );
