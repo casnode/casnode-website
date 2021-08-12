2975e2b9c6fc6cbd991a21b0079d40fa0cf4c697
ErikQQY <2283984853@qq.com>
2021-08-13 01:25:21 +0800

    feat: Add more Casnode iframes in page
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,8 +25,8 @@ function HomepageHeader() {
         </div>
       </div>
       <div className={styles.casnodewindow}>
-      <BrowserWindow url="https://forum.casbin.com" className={styles.BrowserWindow}>
-        <div className={styles.iframediv}>
+      <BrowserWindow url="https://forum.casbin.com">
+        <div className={styles.headeriframe}>
         <iframe className="headerwindow" src="https://forum.casbin.com" width="870px" height="450px" border="0" frameborder="0"></iframe>
         </div>
       </BrowserWindow>
@@ -35,16 +35,52 @@ function HomepageHeader() {
   );
 }
 
-function CasnodeWindow() {
+function LoginContent() {
   return (
-    <div className={styles.casnodewindow}>
+    <div className={styles.logincontent}>
     <div className="container text--center">
+      <div className="row">
+        <div className="col">
+        Casnode is a forum developed by <Link href="https://github.com/casbin/casnode">Casbin community</Link> with a modern interface.
+        <br />
+        With supporting many third-party application login, you can choose your favorite social network to login.
+        </div>
+        <div className={styles.loginwindow}>
         <BrowserWindow url="https://forum.casbin.com">
-        <iframe src="https://forum.casbin.com" width="870" height="500" frameborder="0"></iframe>
-      </BrowserWindow>
+        <div className={styles.loginiframediv}>
+        <iframe className={styles.loginiframe} src="https://door.casbin.com/login/oauth/authorize?client_id=014ae4bd048734ca2dea&response_type=code&redirect_uri=https://forum.casbin.com/callback&scope=read&state=app-casbin-forum" width="870px" height="700px" border="0" frameborder="0"></iframe>
+        </div>
+        </BrowserWindow>
+      </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+function LanguageContent() {
+  return (
+    <div className={styles.languagecontent}>
+    <div className="container text--center">
+      <div className="row">
+      <div className={styles.languagewindow}>
+        <BrowserWindow url="https://forum.casbin.com">
+          <div className={styles.languageiframediv}>
+        <iframe className={styles.languageiframe} src="https://forum.casbin.com/select/language" width="870px" height="450px" scrolling="no" frameborder="0"></iframe>
+        </div>
+        </BrowserWindow>
+        </div>
+        <div className="col">
+          <font size="6" color="red">
+          你好 
+          </font>&nbsp;<font size="6" color="grey">Hello</font>&nbsp;<font size="6" color="green">Bonjour</font><br /> <font size="6" color="blue">こんにちは</font> <font size="6" color="yellow">안녕하세요</font> <font size="6" color="purple">Hallo</font><br /><font size="6" color="orange">привет</font>
+          <br />
+          With supporting for many languages, you can choose your language to access Casnode without barrier
         </div>
       </div>
-  );
+    </div>
+  </div>
+  )
 }
 
 export default function Home() {
@@ -56,6 +92,8 @@ export default function Home() {
       <HomepageHeader />
       <main>
         <HomepageFeatures />
+        <LoginContent />
+        <LanguageContent />
       </main>
     </Layout>
   );
