e35f6115e224a0a264bf5328bf24b1efec324be6
YunShu <im@yunshu.site>
2022-09-20 23:24:57 +0800

    feat: Use the fastest Casnode demo site (#69)

--- src/theme/NavbarItem/ComponentTypes.js
+++ src/theme/NavbarItem/ComponentTypes.js
@@ -1,8 +1,8 @@
 import ComponentTypes from "@theme-original/NavbarItem/ComponentTypes";
-import Casnode from "../../components/NavbarItems/Casnode";
+import CustomNavbarItem from "../../components/NavbarItems";
 
 // see https://github.com/facebook/docusaurus/issues/7227
 export default {
   ...ComponentTypes,
-  "custom-casnode": Casnode,
+  "custom-item": CustomNavbarItem,
 };
