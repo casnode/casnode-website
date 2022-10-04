c03a7173e62bc9cc1795efd9e4c7e9b92a1ac3e7
YunShu <im@yunshu.site>
2022-10-04 16:57:02 +0800

    feat: mobile adaptation

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,10 @@ import HomepageFeatures from "../components/HomepageFeatures";
 import BrowserWindow from "../components/BrowserWindow";
 import Translate from "@docusaurus/Translate";
 import CasnodeCard from "../components/CasnodeCard";
+import {useWindowSize} from "@docusaurus/theme-common";
 
 function HomepageHeader() {
+  const isMobile = useWindowSize();
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
@@ -31,18 +33,21 @@ function HomepageHeader() {
           </Link>
         </div>
       </div>
-      <div className={styles.casnodewindow}>
-        <BrowserWindow>
-          <div className={styles.headeriframe}>
-            <CasnodeCard int="https://forum.casbin.org" cn="https://forum.casbin.com" scrolling="yes" />
-          </div>
-        </BrowserWindow>
-      </div>
+      {isMobile === "mobile" ||
+        <div className={styles.casnodewindow}>
+          <BrowserWindow>
+            <div className={styles.headeriframe}>
+              <CasnodeCard int="https://forum.casbin.org" cn="https://forum.casbin.com" scrolling="yes" />
+            </div>
+          </BrowserWindow>
+        </div>
+      }
     </header>
   );
 }
 
 function LoginContent() {
+  const isMobile = useWindowSize();
   return (
     <div className={styles.logincontent}>
       <div className="container text--center">
@@ -51,19 +56,22 @@ function LoginContent() {
             <Translate>Casnode is a forum developed by</Translate> <Link href="https://github.com/casbin/casnode"><Translate>Casbin community</Translate></Link> <Translate>with a modern interface.</Translate>
             <br />
             <Translate>
-        With supporting many third-party application login, you can choose your favorite social network to login.
+              With supporting many third-party application login, you can choose your favorite social network to login.
             </Translate>
           </div>
-          <div className={styles.loginwindow}>
-            <BrowserWindow>
-              <div className={styles.loginiframediv}>
-                <CasnodeCard width="870px" height="720px"
-                  int="https://door.casdoor.com/login/oauth/authorize?client_id=014ae4bd048734ca2dea&response_type=code&redirect_uri=https%3A%2F%2Fforum.casbin.org%2Fcallback&scope=read&state=app-casnode"
-                  cn="https://door.casbin.com/login/oauth/authorize?client_id=014ae4bd048734ca2dea&response_type=code&redirect_uri=https://forum.casbin.com/callback&scope=read&state=app-casbin-forum"
-                />
-              </div>
-            </BrowserWindow>
-          </div>
+          {
+            isMobile === "mobile" ||
+            <div className={styles.loginwindow}>
+              <BrowserWindow>
+                <div className={styles.loginiframediv}>
+                  <CasnodeCard width="870px" height="720px"
+                    int="https://door.casdoor.com/login/oauth/authorize?client_id=014ae4bd048734ca2dea&response_type=code&redirect_uri=https%3A%2F%2Fforum.casbin.org%2Fcallback&scope=read&state=app-casnode"
+                    cn="https://door.casbin.com/login/oauth/authorize?client_id=014ae4bd048734ca2dea&response_type=code&redirect_uri=https://forum.casbin.com/callback&scope=read&state=app-casbin-forum"
+                  />
+                </div>
+              </BrowserWindow>
+            </div>
+          }
         </div>
       </div>
     </div>
@@ -71,27 +79,31 @@ function LoginContent() {
 }
 
 function LanguageContent() {
+  const isMobile = useWindowSize();
   return (
     <div className={styles.languagecontent}>
       <div className="container text--center">
         <div className="row">
-          <div className={styles.languagewindow}>
-            <BrowserWindow>
-              <div className={styles.languageiframediv}>
-                <CasnodeCard
-                  int="https://forum.casbin.org/select/language"
-                  cn="https://forum.casbin.com/select/language"
-                />
-              </div>
-            </BrowserWindow>
-          </div>
+          {
+            isMobile === "mobile" ||
+            <div className={styles.languagewindow}>
+              <BrowserWindow>
+                <div className={styles.languageiframediv}>
+                  <CasnodeCard
+                    int="https://forum.casbin.org/select/language"
+                    cn="https://forum.casbin.com/select/language"
+                  />
+                </div>
+              </BrowserWindow>
+            </div>
+          }
           <div className="col">
             <font size="6" color="red">
-          你好
+              你好
             </font>&nbsp;<font size="6" color="grey">Hello</font>&nbsp;<font size="6" color="green">Bonjour</font><br /> <font size="6" color="blue">こんにちは</font> <font size="6" color="yellow">안녕하세요</font> <font size="6" color="purple">Hallo</font><br /><font size="6" color="orange">привет</font>
             <br />
             <Translate>
-          With supporting for many languages, you can choose your language to access Casnode without barrier
+              With supporting for many languages, you can choose your language to access Casnode without barrier
             </Translate>
           </div>
         </div>
